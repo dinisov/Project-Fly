f917b573139071c6731c06ee927f1565174f0d8f
dinisov <dgokaydin@gmail.com>
2023-05-30 12:56:27 +1000

    COmpleted LED experiment script

--- Python/LED Sequential Effects/led_SEs.py
+++ Python/LED Sequential Effects/led_SEs.py
@@ -1,11 +1,16 @@
-from gpiozero import PWMLED
+from gpiozero import PWMLED, OutputDevice
 from time import sleep
 import numpy as np
 
+## pins for the two separate LEDs
 led1 = PWMLED(17)
 led2 = PWMLED(27)
 
-#pulse width modulation for both LEDs
+# pins to start the 2P and close/open the shutter
+shutterPin = OutputDevice(5)
+framePin = OutputDevice(13)
+
+#LED intensity by PWM
 pwm1 = .1
 pwm2 = .1
 
@@ -17,7 +22,7 @@ stimOff = .7
 numberOfBlocks = 2
 blockLength = 5
 
-interBlockPeriod = 2
+recordingPeriod = 2
 
 sequenceLength = numberOfBlocks*blockLength
 
@@ -28,20 +33,37 @@ np.random.shuffle(randomSequence)
 
 trial = 0
 
+# probably should pulse the shutter pin
+
 for i in range(numberOfBlocks):
 
+	# display part
+	framePin.off()#stop recording
+	shutterPin.off()#close shutter
+
 	for s in range(blockLength):
-
-		led1.value = pwm1*randomSequence[trial]
-		led2.value = pwm2*(1-randomSequence[trial])
-
-		sleep(stimOn)
 
 		led1.value = 0
 		led2.value = 0
 
 		sleep(stimOff)
 
+		led1.value = pwm1*randomSequence[trial]
+		led2.value = pwm2*(1-randomSequence[trial])
+
+		sleep(stimOn)
+
 		trial += 1
 
-	sleep(interBlockPeriod)
+	led1.value = 0
+	led2.value = 0
+
+	#recording part
+
+	# open shutter and start recording
+	shutterPin.on()#open shutter
+	sleep(0.020)#enough time for shutter to be fully open
+	framePin.on()#start recording
+
+
+	sleep(recordingPeriod)
